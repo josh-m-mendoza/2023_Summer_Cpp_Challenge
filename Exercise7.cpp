418e8191b87839df5ec6298aa4b9b548cd5b97d1
Josh Mendoza <joshuamichaelmendoza4@gmail.com>
2026-01-04 22:42:06 -0600

    Exercise8 solution

--- Exercise7.cpp
+++ Exercise7.cpp
@@ -1,2 +1,33 @@
 //Title: Reverse a String
 //Description: 	Write a program that asks the user to enter a string and then prints the reverse of that string.	
+
+#include <iostream>
+
+std::string my_reverse(std::string string_to_reverse);
+
+int main(){
+
+    // variables used
+
+    std::string str;
+
+    std::cout << "Enter a string to reverse: \n";
+    std::getline(std::cin, str);
+
+    std::string result; 
+    
+    // how can I validate string input??
+
+    result = my_reverse(str);
+    std::cout << "The reverse of " << str << " is " << result << std::endl;
+    return 0;
+}
+
+std::string my_reverse(std::string string_to_reverse){
+    std::string new_str;
+
+    for (int i = string_to_reverse.length(); i >= 0; i--) {
+        new_str.push_back(string_to_reverse[i]);
+    }
+    return new_str;
+}
